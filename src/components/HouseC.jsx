57aca731acae09af5530daca458725859d60e401
felcross <feljava32@gmail.com>
2021-09-06 23:31:02 -0300

    testanto git auth

--- src/components/HouseC.jsx
+++ src/components/HouseC.jsx
@@ -6,7 +6,7 @@ export default function HouseC(props) {
     return (
         <div className={styles.date}>
            <div>
-                <Image src={pic} width={'150%'} height={'80vh'}  quality={90} alt='casa'/>
+                <Image src={pic} width={'150%'} height={'80vh'}  quality={90} alt='Brasão'/>
                 </div>      
             <div className={styles.info}>
 
